fb9dba9cf6e5c5be270bcd8710d651aef55d47e3
Ray Garner <ray@perfectcast.co.uk>
2020-04-13 19:36:18 +0100

    fixed .mix.sh

--- .mix.sh
+++ .mix.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#./.mix.sh <trackname> <trackname> <caller folder>
-ffmpeg -i "$1.wav" -i "$2.wav" -filter_complex amerge=inputs=2 -ac 2 "$3/mixes/$1+$2.wav"
+#./.mix.sh <trackname> <trackname>
+ffmpeg -i "raw/$1.wav" -i "raw/$2.wav" -filter_complex amerge=inputs=2 -ac 2 "mixes/$1+$2.wav"
