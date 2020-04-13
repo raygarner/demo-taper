903ceff58ff768540d2ff4f5c0a5c4720f203d68
Ray Garner <ray@perfectcast.co.uk>
2020-04-13 18:40:36 +0100

    fixed case statement and folder name thing

--- .record.sh
+++ .record.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# ./.record.sh <trackname> <caller project>
-ffmpeg -f pulse -i default "$2/raw/$1.wav"
+# ./.record.sh <trackname>
+ffmpeg -f pulse -i default "raw/$1.wav"
--- .start.sh
+++ .start.sh
@@ -4,24 +4,24 @@ NAME=`cat name`
 while [ 1 ]; do
     read -p '> ' COMMAND ARG1 ARG2
 
-    case COMMAND in
-        record)
-            ../.record.sh "$ARG1" "$NAME"
+    case "$COMMAND" in
+        "record")
+            ../.record.sh "$ARG1"
             ;;
 
-        mix)
-            ../.mix.sh "$ARG1" "$ARG2" "$NAME"
+        "mix")
+            ../.mix.sh "$ARG1" "$ARG2"
             ;;
 
-        quit)
+        "quit")
             exit
             ;;
 
-        bpm)
+        "bpm")
             echo "$ARG1" > bpm
             ;;
 
-        ?)
+        "?")
             ../.help.sh
             ;;
 
