1438004d59793312e20c983d2da1d95cd1fb5420
Ray Garner <ray@perfectcast.co.uk>
2020-04-13 18:14:38 +0100

    interface dev

--- .start.sh
+++ .start.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 while [ 1 ]; do
-    read -p '> ' INPUT
+    read -p '> ' COMMAND ARG1 ARG2
 
-    case INPUT in
-        record*)
+    case COMMAND in
+        record)
+            ../.record "$ARG1"
             ;;
 
-        mix*)
+        mix)
+            ../.mix "$ARG1" "$ARG2"
+            ;;
 
-
-
+        quit)
+            exit
+            ;;
 
         *)
     esac
